27ef7270323548d61b755c7a7114ee84e3320dbd
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-10-31 16:40:58 +0530

    added build on heroku action

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,6 +1,7 @@
 <!doctype html>
 
 <head>
+    <title>Blade Icons Package List</title>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="/css/app.css" rel="stylesheet">
